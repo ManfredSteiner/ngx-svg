fe250ce24de88bec3688907b2a9c4f869ce3edb3
Manfred Steiner <sx@htl-kaindorf.at>
2017-12-08 16:32:33 +0100

    step4

--- src/app/logo.component.ts
+++ src/app/logo.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 
 @Component({
     selector: 'app-logo',
     templateUrl: 'logo.component.html',
     styleUrls: [ 'logo.component.css' ]
 })
-export class LogoComponent {
+export class LogoComponent implements OnInit {
 
-    constructor () {}
+    @ViewChild('left') left: ElementRef;
+    @ViewChild('right') right: ElementRef;
 
+    ngOnInit() {
+        TweenMax.to(this.left.nativeElement, 1, {
+            attr: {
+                points: '125,30 125,30 125,30 31.9,30 31.9,230 125,230 125,230 125,230 203.9,186.3 218.1,63.2'
+            },
+            repeat: -1,
+            yoyo: true,
+            ease: Cubic.easeInOut
+        });
+        TweenMax.to(this.right.nativeElement, 1, {
+            attr: {
+                points: '125,30 125,52.2 125,52.1 125,153.4 125,153.4 125,230 125,230 218.1,230 218.1,30 125,30'
+            },
+            repeat: -1,
+            yoyo: true,
+            ease: Cubic.easeInOut
+        });
+    }
 }
