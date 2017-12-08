93f3357352480fe415d59e53ecf7d4b85384a925
Manfred Steiner <Manfred.Steiner@gmx.at>
2017-12-08 12:36:56 +0100

    step1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,11 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
-
+import { LogoComponent } from './logo.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent, LogoComponent
   ],
   imports: [
     BrowserModule
